03c176b0d6aaaa5b8e14ee6f60b253f1d6e1528e
LuisElcure <risewavetech@gmail.com>
2026-05-14 07:59:16 -0300

    Start sets lesson

--- python/11_classes.py
+++ python/11_classes.py
@@ -3,7 +3,10 @@
 class Person:
     #Método constructor de clases.
     def __init__(self, name, surname, alias="Sin alias"):
-        self.full_name=f"{name} {surname}"
+        self.full_name=f"{name} {surname}"#propiedad púb}}}}}}}}lica
+        
+        self.__name = name #Crear una propiedad privada
+
         #Función dentro de la clase.
         def walk(self):
             print(f"{self.full_name} está caminando")
